4325d825770eeb348acf544ad97d7680ba6090a8
MaFeLP <mafelp@proton.me>
2025-04-04 12:12:15 +0200

    Footer now better clickable
    Signed-off-by: MaFeLP <mafelp@proton.me>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,9 +18,12 @@ const Footer = () => {
                     </div>
                     <div>
                         <h3 className="text-lg font-bold mb-4">Contact Us</h3>
-                        <p>Email: <a className="hover:text-indigo-300" href="mailto:support@noctura-pillow.store">support@noctura-pillow.store</a></p>
-                        <p>Phone (North America): <a className="hover:text-indigo-300" href="tel:+15550173">+1 (555) 0173</a></p>
-                        <p>Phone (Europe): <a className="hover:text-indigo-300" href="tel:+4952116391643">+49 521 16391643</a></p>
+                        <p><a className="hover:text-indigo-300" href="mailto:support@noctura-pillow.store">Email:
+                            support@noctura-pillow.store</a></p>
+                        <p><a className="hover:text-indigo-300" href="tel:+15550173">Phone (North America): +1 (555)
+                            0173</a></p>
+                        <p><a className="hover:text-indigo-300" href="tel:+4952116391643">Phone (Europe): +49 521
+                            16391643</a></p>
                         <div className="flex space-x-4 mt-4">
                             <a className="hover:text-indigo-300" href="https://www.instagram.com/noctura.pillow/">Instagram</a>
                             <a className="hover:text-indigo-300" href="https://www.tiktok.com/@noctura.pillow">TikTok</a>
